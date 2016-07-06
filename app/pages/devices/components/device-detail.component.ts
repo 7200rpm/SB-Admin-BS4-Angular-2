e4f1493454708b98af4fd40875ed586fb9867b9c
Gregory Eslinger <gregjesl@gmail.com>
2016-07-06 16:23:13 -0700

    Built the html for the detailed Device page.

--- app/pages/devices/components/device-detail.component.ts
+++ app/pages/devices/components/device-detail.component.ts
@@ -6,17 +6,76 @@ import { Router, ActivatedRoute }       from '@angular/router';
 @Component({
   selector: 'device-detail-cmp',
   template: `
-    <div *ngIf="device">
-      <h2>{{device.first_name}} {{device.last_name}}</h2>
-      <div><label>id: </label>{{device.customerID}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="device.first_name" placeholder="name"/>
+    <div class="container-fluid" *ngIf="device">
+      <div class="row">
+        <h2>Device {{device.serial_number}}
+          <button type="button" class="btn btn-lg btn-primary" (click)="goBack()">Back</button>
+        </h2>
+        <div class="col-xl-3">
+          <h3>Update</h3>
+          <form role="form">
+            <fieldset class="form-group">
+              <label>Serial Number</label>
+              <input class="form-control" value="{{device.serial_number}}" [(ngModel)]="device.serial_number">
+            </fieldset>
+            <fieldset class="form-group">
+              <label>Particle ID</label>
+              <input class="form-control" value="{{device.particleID}}" [(ngModel)]="device.particleID">
+            </fieldset>
+            <fieldset class="form-group">
+              <label>Hardware Revision</label>
+              <input class="form-control" value="{{device.hardware_revision}}" [(ngModel)]="device.hardware_revision">
+            </fieldset>
+            <fieldset class="form-group">
+              <label>Notes</label>
+              <textarea class="form-control" rows="3" value="{{device.notes}}" [(ngModel)]="device.notes"></textarea>
+            </fieldset>
+            <button type="submit" class="btn btn-secondary">Submit</button>
+            <button type="reset" class="btn btn-secondary">Reset</button>
+          </form>
+        </div>
+        <div class="col-xl-6">
+          <h3>Telemetry</h3>
+          <form role="form">
+            <div class="form-group input-group">
+              <input type="text" class="form-control" placeholder="Search measurands">
+              <span class="input-group-btn"><button class="btn btn-secondary" type="button"><i class="fa fa-search"></i></button></span>
+            </div>
+          </form>
+          <div class="card card-block">
+            <div class="table-responsive">
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th>Timestamp</th>
+                    <th>Measurand</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <!-- *ngFor="let t of devices.telemetry" -->
+                  <tr>
+                    <td>{{t.timestamp}}</td>
+                    <td>{{t.measurand}}</td>
+                    <td>{{t.value}}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <h3>Wakeups</h3>
+            <ul class="list-group">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        </div>
       </div>
     </div>
-    <p>
-      <button (click)="goBack()">Back</button>
-    </p>
   `
 })
 
