6aacb30af0ed5ed87bdc579a47a2290b23326ff4
Gregory Eslinger <gregjesl@gmail.com>
2016-07-18 19:04:27 -0700

    Updated devices and telemetry pages

--- app/pages/devices/components/device-detail.component.ts
+++ app/pages/devices/components/device-detail.component.ts
@@ -7,55 +7,57 @@ import { Router, ActivatedRoute }       from '@angular/router';
   selector: 'device-detail-cmp',
   template: `
     <div class="container-fluid" *ngIf="device">
-        <div class="row">
+    <div class="row">
         <div class="col-xl-12">
-        <h2 class="page-header"><button type="button" class="btn btn-primary" (click)="goBack()">Back</button> | <i class="fa fa-barcode"></i> {{device.serial_number}}
-        </h2>
-        <hr />
+            <h2 class="page-header"><button type="button" class="btn btn-primary" (click)="goBack()">Back</button> | <i class="fa fa-barcode"></i>                {{device.serial_number}}
+            </h2>
+            <hr />
         </div>
     </div>
-      <div class="row">
+    <div class="row">
         <div class="col-xl-3">
-          <div class="card card-primary card-inverse">
-            <div class="card-header card-primary">
-              <h3>Update</h3>
-            </div>
-            <div class="card-block bg-white">
+            <div class="card card-primary card-inverse">
+                <div class="card-header card-primary">
+                    <h3>Update</h3>
+                </div>
+                <div class="card-block bg-white">
 
-          <form role="form">
-            <fieldset class="form-group">
-              <label>Nickname</label>
-              <input class="form-control" value="{{device.nickname}}" [(ngModel)]="device.nickname">
-            </fieldset>
-            <fieldset class="form-group">
-              <label>Serial Number</label>
-              <input class="form-control" value="{{device.serial_number}}" [(ngModel)]="device.serial_number">
-            </fieldset>
-            <fieldset class="form-group">
-              <label>Particle ID</label>
-              <input class="form-control" value="{{device.particleID}}" [(ngModel)]="device.particleID">
-            </fieldset>
-            <fieldset class="form-group">
-              <label>Hardware Revision</label>
-              <input class="form-control" value="{{device.hardware_revision}}" [(ngModel)]="device.hardware_revision">
-            </fieldset>
-            <fieldset class="form-group">
-              <label>Notes</label>
-              <textarea class="form-control" rows="3" value="{{device.notes}}" [(ngModel)]="device.notes"></textarea>
-            </fieldset>
-            <button type="submit" class="btn btn-secondary">Submit</button>
-            <button type="reset" class="btn btn-secondary">Reset</button>
-          </form>
-        </div>
-        </div>
-          <div *ngIf=device.orderID class="card card-success card-inverse">
+                    <form role="form">
+                        <fieldset class="form-group">
+                            <label>Nickname</label>
+                            <input class="form-control" value="{{device.nickname}}" [(ngModel)]="device.nickname">
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label>Serial Number</label>
+                            <input class="form-control" value="{{device.serial_number}}" [(ngModel)]="device.serial_number">
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label>Particle ID</label>
+                            <input class="form-control" value="{{device.particleID}}" [(ngModel)]="device.particleID">
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label>Hardware Revision</label>
+                            <input class="form-control" value="{{device.hardware_revision}}" [(ngModel)]="device.hardware_revision">
+                        </fieldset>
+                        <fieldset class="form-group">
+                            <label>Notes</label>
+                            <textarea class="form-control" rows="3" value="{{device.notes}}" [(ngModel)]="device.notes"></textarea>
+                        </fieldset>
+                        <button type="submit" class="btn btn-secondary">Submit</button>
+                        <button type="reset" class="btn btn-secondary">Reset</button>
+                    </form>
+                </div>
+            </div>
+            <div *ngIf=device.orderID class="card card-success card-inverse">
                 <div class="card-header card-success">
                     <div class="row">
                         <div class="col-xs-3">
                             <i class="fa fa-user fa-5x"></i>
                         </div>
                         <div class="col-xs-9 text-xs-right">
-                            <div><h4>Shipped to<br />{{device.order.name}}</h4></div>
+                            <div>
+                                <h4>Shipped to<br />{{device.order.name}}</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -74,50 +76,72 @@ import { Router, ActivatedRoute }       from '@angular/router';
                             <i class="fa fa-home fa-5x"></i>
                         </div>
                         <div class="col-xs-9 text-xs-right">
-                            <div><h3>In Stock</h3></div>
+                            <div>
+                                <h3>In Stock</h3>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-xl-9">
-          <div class="card card-block">
-            <h3>Events</h3>
-            <iframe src="http://wakeapi.azurewebsites.net/charts/events.php" 
-              style="border:none" width="100%" height="1200px" onload="resizeIframe(this)" ></iframe>
-          </div>
-          <div class="card card-block">
-          <h3>Telemetry</h3>
-          <form role="form">
-            <div class="form-group input-group">
-              <input type="text" class="form-control" placeholder="Search measurands">
-              <span class="input-group-btn"><button class="btn btn-secondary" type="button"><i class="fa fa-search"></i></button></span>
+            <div class="card card-block">
+                <h3>Events</h3>
+                Timeline of recent events for the device. 
             </div>
-          </form>
-            <div class="table-responsive">
-              <table class="table table-hover">
-                <thead>
-                  <tr>
-                    <th>Timestamp</th>
-                    <th>Event</th>
-                    <th>Measurement</th>
-                    <th>Value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr *ngFor="let event of device.events" (click)="onSelect(d)">
-                    <td>{{event.published_at}}</td>
-                    <td>{{event.name}}</td>
-                    <td>{{event.measurand}}</td>
-                    <td>{{event.value}}</td>
-                  </tr>
-                </tbody>
-              </table>
+            <div class="card card-block">
+                <h3>Voltage</h3>
+                Timeline of recent events for the device. 
             </div>
-          </div>
         </div>
-      </div>
     </div>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card card-block">
+                <h3>Scans</h3>
+                <div class="col-xl-4">
+                    Table of recent scans
+                </div>
+                <div class="col-xl-8">
+                    Plot of selected scan
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card card-block">
+                <h3>Telemetry</h3>
+                <form role="form">
+                    <div class="form-group input-group">
+                        <input type="text" class="form-control" placeholder="Search measurands">
+                        <span class="input-group-btn"><button class="btn btn-secondary" type="button"><i class="fa fa-search"></i></button></span>
+                    </div>
+                </form>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Timestamp</th>
+                                <th>Event</th>
+                                <th>Measurement</th>
+                                <th>Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr *ngFor="let event of device.events" (click)="onSelect(d)">
+                                <td>{{event.published_at}}</td>
+                                <td>{{event.name}}</td>
+                                <td>{{event.measurand}}</td>
+                                <td>{{event.value}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   `
 })
 
