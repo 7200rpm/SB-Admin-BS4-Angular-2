2f160a0f03f78627b86dc367feea88b4af87b1a0
Brandon Brown <7200rpm@gmail.com>
2016-07-23 22:31:29 -0700

    Fixed power table
    -Added directive to class

--- app/pages/customers/components/customer-detail.component.ts
+++ app/pages/customers/components/customer-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, AfterViewInit} from '@angular/core';
 import {CustomerService} 			from '../customer.service'
 import { Customer } from '../customer';
 import { Router, ActivatedRoute }       from '@angular/router';
